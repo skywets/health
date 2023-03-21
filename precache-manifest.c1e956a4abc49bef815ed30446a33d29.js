618039c1f52df6440f1877519bcc7d32d522d8a6
skywets <ozodsamobahromov777@mail.tu>
2023-03-21 22:07:50 +0300

    Updates

---
+++ precache-manifest.c1e956a4abc49bef815ed30446a33d29.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e67836e558890806f0e",
+    "url": "/health/static/css/main.5c98f5a5.chunk.css"
+  },
+  {
+    "revision": "9e67836e558890806f0e",
+    "url": "/health/static/js/main.9e67836e.chunk.js"
+  },
+  {
+    "revision": "f2078eef1e2db74d1059",
+    "url": "/health/static/css/1.41a4c11a.chunk.css"
+  },
+  {
+    "revision": "f2078eef1e2db74d1059",
+    "url": "/health/static/js/1.f2078eef.chunk.js"
+  },
+  {
+    "revision": "7522bd96d7a641ecee74",
+    "url": "/health/static/js/runtime~main.7522bd96.js"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/health/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/health/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/health/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/health/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/health/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/health/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/health/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/health/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/health/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/health/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/health/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/health/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/health/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/health/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/health/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "3c3b1231361e885a7778487fa4eac94f",
+    "url": "/health/index.html"
+  }
+];
